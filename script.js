83895f74b8e8c2efd854d253221616d15d9eef08
mariem kamaych <kamaychmeryem@gmail.com>
2023-09-20 20:55:54 +0100

    add some changes

--- script.js
+++ script.js
@@ -13,7 +13,42 @@ for(i=0;i<400;i++){
     let div = document.createElement("div"); 
     div.className = "cell";
     div.id=i; 
+    div.setAttribute("onclick", "game1(this.id)");
     game.appendChild(div); 
 
+
+
+
 }
+
+let squares =[];
+
+function winner(){
+    for(i=0;i<400;i++){
+
+        squares[i]=document.getElementById(i).innerHTML;
+
+
+    }
+    console.log(squares);
+
+
+}
+
+
+let turn="X";
+
+function game1(id){
+    let element = document.getElementById(id);
+    if(turn === "X" && element.innerHTML == ""){
+        element.innerHTML="X";
+        turn="O";
+    }else if(turn === "O" && element.innerHTML == ""){
+        element.innerHTML="O";
+        turn="X";
+    }
+    winner();
+}
+
+
 
