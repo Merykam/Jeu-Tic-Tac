67a99d9c7e72d82b6cc4ea9ebfa2907b08298b0a
mariem kamaych <kamaychmeryem@gmail.com>
2023-09-22 17:03:44 +0100

    add some fonctionality

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let game= document.querySelector(".game-container");
+let game = document.querySelector(".game-container");
 
 let container = document.querySelector(".container");
 
-console.log(game);
+
+let winnerTitle = document.querySelector(".winner h1");
+
+// console.log(game);
 
 let i;
 let turn = "X";
@@ -15,7 +18,7 @@ let matrix = [];
 // function checkConsecutive(turn) {
 //     const rows = 20;
 //     const cols = 20;
-  
+
 //     // Check horizontally
 //     for (let row = 0; row < rows; row++) {
 //       for (let col = 0; col <= cols - 5; col++) {
@@ -31,190 +34,179 @@ let matrix = [];
 //         }
 //       }
 //     }
-  
-   
-  
+
+
+
 //     return false;
 //   }
 
 
 
 
-for(i=0;i<400;i++){
-   
-  let div = document.createElement("div"); 
+for (i = 1; i <= 400; i++) {
+
+  let div = document.createElement("div");
   div.className = "cell";
-  div.id=i; 
+  div.id = i;
   div.setAttribute("onclick", "game1(this.id)");
-  game.appendChild(div); 
+  game.appendChild(div);
 
 
 
 
 }
-  
-
-var squares =[];
 
 
-// function run(turn){
-
-//   const hasConsecutive = winner(turn);
-  
-//   if (hasConsecutive) {
-//     // console.log(`Five consecutive squares with content ${contentToCheck} found.`);
-//     console.log(`Five consecutive squares with content found.`);
-    
-//   } else {
-//     // console.log(`No five consecutive squares with content ${contentToCheck} found.`);
-//     console.log(`No five consecutive squares with content found.`);
-//   }
-
-// }
-//   const contentToCheck = 2; // Change this to the content you want to check
-
-  
+var squares = [];
 
 
 
+function winner(turn,row) {
+
+  matrix = [];
+  for (i = 1; i <= 400; i++) {
+
+    squares[i-1] = document.getElementById(i).innerHTML;
 
 
+  }
+  // console.log(squares);
 
 
+  if (squares.length === 400) {
 
 
+    for (let i = 0; i < 20; i++) {
 
 
+      let chunk = squares.slice(i * 20, (i + 1) * 20);
 
-
-
- 
-
-
-function winner(turn){
-
-  matrix=[];
-    for(i=0;i<400;i++){
-       
-        squares[i]=document.getElementById(i).innerHTML;
-            
-
-    }
-    console.log(squares);
-
-
-    if (squares.length === 400) {
-      
-    
-        for (let i = 0; i < 20; i++) {
-          
-        
-        let chunk = squares.slice(i * 20, (i + 1) * 20);
-
-        matrix.push(chunk);
-        }
-
-         console.log(matrix);
-
-//test
-
-
-  
-
-
-// en test
-
-
-
-    } else {
-        console.error("The original array must have exactly 400 elements.");
+      matrix.push(chunk);
     }
 
+    // console.log(matrix);
 
-    
-// function checkConsecutive(turn,matrix) {
-    const rows = 20;
-    const cols = 20;
-    let count=0;
-  
-    // Check horizontally
-    for (let row = 0; row < rows; row++) {
-      for (let col = 0; col <= cols - 5; col++) {
-        // let consecutive = true;
-      // console.log(matrix);
-        for (let i = 0; i < 5; i++) {
-          if (matrix[row] && matrix[row][col + i] == turn) {
 
-             console.log(turn,matrix[row][col + i]);
-            count++;
-            if(count>=5){
-              break;
-            }
-            // 
-            // consecutive = false;
-            // break;
-          }else{
-            count=0;
-          }
-          // else if (matrix[row] && matrix[row][col + i] == turn) {
-            
-          //   // return true;
-          // }
-        }
 
-        if(count>=5){
-          break;
-          
-      }
-        // if (consecutive) {
-        //   console.log(`Five consecutive squares with content found.`);
-        //   // return true;
-        // }
-      }
-      if(count>=5){
-          
-         console.log(`Five consecutive squares with content found.`);
-        break;
-      }else{
-         console.log(`Five consecutive squares with content nooot found.`);
-      }
-    }
-  
-   
-  
-    // return false;
+
+  } else {
+    console.error("The original array must have exactly 400 elements.");
   }
 
 
-// }
+
+  // const rows = 20;
+  const cols = 20;
+  let count = 0;
+
+  // Check horizontally
+  // for (let row = 0; row < rows; row++) {
+    
+    for (let col = 0; col <= cols - 5; col++) {
+
+      for (let i = 0; i < 5; i++) {
+
+        if (matrix[row] && matrix[row][col + i] == turn) {
+
+          // console.log(turn, matrix[row][col + i]);
+
+          count++;
 
 
+          if (count >= 5) {
+            break;
+          }
 
+        } else{
 
-// let turn="X";
+          break;
+          
+        }
 
+      }
 
-function game1(id){
-    let element = document.getElementById(id);
-    if(turn === "X" && element.innerHTML == ""){
-        element.innerHTML="X";
-        console.log(turn);
-        winner(turn);
-        // run(turn);
-        // checkConsecutive(turn);
-        turn="O";
+      if (count >= 5) {
+        break;
 
-    }else if(turn === "O" && element.innerHTML == ""){
-        element.innerHTML="O";
+      }else {
+        count = 0;
+      }
 
-        console.log(turn);
-        winner(turn);
-        // run(turn);
-        // checkConsecutive(turn);
-        turn="X";
     }
+    if (count >= 5) {
+
+
+      winnerTitle.innerHTML=`${turn} is the winner`;
+
+      console.log(`Five consecutive squares with content found.`);
+      // break;
+    } else {
+      count=0;
+      
+      console.log(`Five consecutive squares with content nooot found.`);
+    }
+  // }
+
+
 
 }
+
+
+
+
+
+function game1(id) {
+  let element = document.getElementById(id);
+  let row = Math.ceil(id/20)-1;
+
+  console.log(row);
+  if (turn === "X" && element.innerHTML == "") {
+    element.innerHTML = "X";
+    
+    // console.log(turn);
+    winner(turn,row);
+    // run(turn);
+    // checkConsecutive(turn);
+    turn = "O";
+
+  } else if (turn === "O" && element.innerHTML == "") {
+    element.innerHTML = "O";
+
+    // console.log(turn);
+    winner(turn,row);
+    // run(turn);
+    // checkConsecutive(turn);
+    turn = "X";
+  }
+
+}
+
+
+
+
+
+
+// for(let row=0; row<=20; row++){
+
+//   for(let col=0; col<=20; col++){
+
+//     for(let j=0;j<5;j++){
+
+
+//       if(matrix[row][col+j]=="x"){
+
+//         console.log("noice");
+  
+//        }
+
+//     }
+
+     
+
+
+//   }
+
+// }
 
 
 
